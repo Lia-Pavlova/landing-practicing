033d89687c9521a9028bfcdfb6260d9382afa68b
Natalia Pavlova <pavlova_li@ukr.net>
2023-01-05 00:03:25 +0200

    first screen

---
+++ cosmos/pages/index.tsx
@@ -0,0 +1,34 @@
+import { Logo } from '../components/Logo'
+import { SocialNetworks } from '../components/SocialNetworks'
+import { ArtButton } from '../components/ArtButton'
+import { Navigation } from '../components/Navigation'
+import { Title } from '../components/Title'
+
+import cl from 'classnames'
+import styles from '../styles/home.module.scss'
+
+export default function Home() {
+  return (
+    <article>
+      <section className={cl(styles.section, styles.sectionMain)}>
+        <header className={styles.header}>
+          <Logo />
+          <SocialNetworks />
+          <ArtButton onClick={() => {}}>Whitepaper</ArtButton>
+        </header>
+        <Navigation />
+        <div>
+          <Title>From dust to&nbsp;dawn</Title>
+          <ul className={styles.buttonList}>
+            <li>
+              <ArtButton onClick={() => {}}>Mint</ArtButton>
+            </li>
+            <li>
+              <ArtButton onClick={() => {}}>Connect Wallet</ArtButton>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </article>
+  )
+}
