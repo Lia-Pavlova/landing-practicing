210ef58a218412de14069341e3834f15e3974802
Natalia Pavlova <pavlova_li@ukr.net>
2023-01-10 01:24:03 +0200

    landing game release page

---
+++ game-release/src/assets/js/languages.js
@@ -0,0 +1,29 @@
+export const languages = {
+  en: {
+    editions: 'Editions',
+    controller: 'Controller',
+    about: 'About Game',
+    explore: 'Explore',
+    exploregame: 'Explore the God of War series',
+    news: 'News',
+    faq: 'FAQ',
+  },
+  ru: {
+    editions: 'Издания',
+    controller: 'Джойстик',
+    about: 'Об игре',
+    explore: 'Исследуйте',
+    exploregame: 'Исследуйте серию God of War',
+    news: 'Новости',
+    faq: 'Помощь',
+  },
+  ukr: {
+    editions: 'Видавництво',
+    controller: 'Джойстик',
+    about: 'Про гру',
+    explore: 'Досліджуйте ',
+    exploregame: 'Досліджуйте серію God of War',
+    news: 'Новини',
+    faq: 'Допомога',
+  },
+}
